dae2ee996ad1fb4584f03d8460bc78fbc8b148cd
Fran LP <fran.martinez@socialwin.es>
2012-11-06 19:14:27 +0100

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
-  def new
-  end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Bienvenido a SocialWin Analytics!"
+			redirect_to @user # ¿sería lo mismo poner:	render 'show'?
+		else
+			render 'new'
+		end
+	end
 
 	def show
 		@user = User.find(params[:id])
 	end
+
+
+
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,14 +4,45 @@ describe "User Pages" do
 
 	subject { page }
 
-	describe "Pagina de registro" do
+	describe "User New: Pagina de registro" do
+
 		before { visit signup_path }
 
-		it { should have_selector('h1',	text: 'Registro') }
-		it { should have_selector('title', text: full_title('Registro')) }  
-  end
+		describe "Texto de pagina" do
 
-	describe "Pagina de perfil de usuario" do
+			it { should have_selector('h1',	text: 'Registro') }
+			it { should have_selector('title', text: full_title('Registro')) }  
+		end
+
+		describe "Accion Signup" do
+
+	    let(:submit) { "Crear mi cuenta" }
+
+	    describe "con informacion invalida" do
+	      it "should not create a user" do
+	        expect { click_button submit }.not_to change(User, :count)
+	      end
+	    end
+
+	    describe "con informacion valida" do
+	      before do
+	        fill_in "Nombre",         with: "Example User"
+	        fill_in "Email",        with: "user@example.com"
+	        fill_in "Password",     with: "foobar"
+	        fill_in "Confirme el password", with: "foobar"
+	      end
+
+	      it "should create a user" do
+	        expect { click_button submit }.to change(User, :count).by(1)
+	      end
+	    end
+
+		end
+
+	end
+
+
+	describe "User Show: Pagina de perfil" do
 		# introducimos un usuario en la BD de test
 		let(:user) { FactoryGirl.create(:user) }
 
@@ -22,6 +53,21 @@ describe "User Pages" do
 
 		it { should have_selector('h1', text: user.name) }
 		it { should have_selector('title', text: user.name) }
+	end
+
+	describe "User Create:" do
+	end
+
+	describe "User Index:" do
+	end
+
+	describe "User Edit:" do
+	end
+
+	describe "User Update:" do
+	end
+
+	describe "User Destroy:" do
 	end
 
 end
